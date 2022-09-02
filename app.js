73ad678cbb3431a536298bf39e430a9b9e83116c
sonjibony <sonjibony@gmail.com>
2022-09-02 23:26:24 +0600

    categories added

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log(3)
+/*--------------------
+category api fetching
+--------------------*/
+const loadCategories = () =>{
+    const url = `  https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCategories(data.data.news_category))
+    //error handling
+    .catch(error => console.log(error))
+}
+/*--------------------
+  category displaying
+--------------------*/
+const displayCategories = categories =>{
+    const categoryField = document.getElementById('category');
+    //looping through array
+ categories.forEach(category => {
+    //creating and appending categories
+const categoryName = document.createElement('p');
+categoryName.innerText=category.category_name;
+categoryField.appendChild(categoryName);
+});
+}
+
+loadCategories();
